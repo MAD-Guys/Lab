3982cee9d74421e3041cc2e50b0ea00bac8fafb1
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2023-03-21 12:36:58 +0100

    Added first menu's implementation

--- LAB2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
+++ LAB2/app/src/main/java/it/polito/mad/lab2/ShowProfileActivity.kt
@@ -2,10 +2,29 @@ package it.polito.mad.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.profile_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.edit) {
+            println("PENCIL CLICKED!!!")
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
