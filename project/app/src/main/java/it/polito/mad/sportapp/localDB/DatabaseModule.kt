eb17220b9fb2e7cc226c8d0f3f3dc492aeb0dc13
giuseppelazzara98 <s302064@studenti.polito.it>
2023-04-23 19:57:17 +0200

    Some fixes

--- project/app/src/main/java/it/polito/mad/sportapp/localDB/DatabaseModule.kt
+++ project/app/src/main/java/it/polito/mad/sportapp/localDB/DatabaseModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import it.polito.mad.sportapp.localDB.dao.EquipmentDao
 import it.polito.mad.sportapp.localDB.dao.ReservationDao
@@ -18,26 +19,31 @@ class DatabaseModule {
 
     // DAO providers
     @Provides
+    @Singleton
     fun provideUserDao(appDatabase: AppDatabase): UserDao {
         return appDatabase.userDao()
     }
 
     @Provides
+    @Singleton
     fun provideSportDao(appDatabase: AppDatabase): SportDao {
         return appDatabase.sportDao()
     }
 
     @Provides
+    @Singleton
     fun provideSportCenterDao(appDatabase: AppDatabase): SportCenterDao {
         return appDatabase.sportCenterDao()
     }
 
     @Provides
+    @Singleton
     fun provideEquipmentDao(appDatabase: AppDatabase): EquipmentDao {
         return appDatabase.equipmentDao()
     }
 
     @Provides
+    @Singleton
     fun provideReservationDao(appDatabase: AppDatabase): ReservationDao {
         return appDatabase.reservationDao()
     }
@@ -45,8 +51,8 @@ class DatabaseModule {
     // Database provider
     @Provides
     @Singleton
-    fun provideDatabase(context: Context): AppDatabase? {
-        return AppDatabase.getInstance(context)
+    fun provideDatabase(@ApplicationContext appContext : Context): AppDatabase? {
+        return AppDatabase.getInstance(appContext)
     }
 
 }
