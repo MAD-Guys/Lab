a395a57eb0fa54f333121e1ba3d3408fd76a4ff2
michelepistan <82035230+michelepistan@users.noreply.github.com>
2023-04-26 09:31:20 +0200

    Equipment reservation list

--- project/app/src/main/java/it/polito/mad/sportapp/reservation_details/ReservationDetailsActivity.kt
+++ project/app/src/main/java/it/polito/mad/sportapp/reservation_details/ReservationDetailsActivity.kt
@@ -5,15 +5,17 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 import it.polito.mad.sportapp.R
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-
+@AndroidEntryPoint
 class ReservationDetailsActivity : AppCompatActivity() {
 
     //views
@@ -28,7 +30,9 @@ class ReservationDetailsActivity : AppCompatActivity() {
     private lateinit var reservationSportCenterAddress : TextView
     private lateinit var directionsButton : ImageButton
     private lateinit var noEquipmentMessage : TextView
-    private lateinit var equipment : RecyclerView
+    private lateinit var equipment : LinearLayout
+    private lateinit var addButton: ImageButton
+    private lateinit var editButton: ImageButton
     private lateinit var reservationTotalPrice : TextView
 
 
@@ -44,29 +48,12 @@ class ReservationDetailsActivity : AppCompatActivity() {
         vm.reservation.value?.let { setQRCodeView(it, qrCode) }
 
         // Retrieve views
-        reservationNumber = findViewById(R.id.reservationNumber)
-        reservationDate = findViewById(R.id.reservationDate)
-        reservationStartTime = findViewById(R.id.reservationStartTime)
-        reservationEndTime = findViewById(R.id.reservationEndTime)
-        reservationSport = findViewById(R.id.reservationSport)
-        reservationPlayground = findViewById(R.id.reservationPlaygroundName)
-        reservationSportCenter = findViewById(R.id.reservationSportCenter)
-        reservationSportCenterAddress = findViewById(R.id.reservationAddress)
-        directionsButton = findViewById(R.id.directionsButton)
-        noEquipmentMessage = findViewById(R.id.noEquipmentMessage)
-        //TODO: equipment list view
-        reservationTotalPrice = findViewById(R.id.reservationPrice)
+        retrieveViews()
 
         // Initialize values
-        reservationNumber.text = "Reservation number: " + String.format("%010d", vm.reservation.value?.id)
-        reservationDate.text = vm.reservation.value?.date?.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
-        reservationStartTime.text = vm.reservation.value?.startTime?.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
-        reservationEndTime.text = vm.reservation.value?.endTime?.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
-        reservationSport.text = vm.reservation.value?.sportName
-        reservationPlayground.text = vm.reservation.value?.playgroundName
-        reservationSportCenter.text = vm.reservation.value?.sportCenterName
-        reservationSportCenterAddress.text = vm.reservation.value?.location
-        reservationTotalPrice.text = "€ " + String.format("%.2f", vm.reservation.value?.totalPrice)
+        initializeValues()
+
+        initializeEquipment()
 
         //TODO: uncomment this block and delete noEquipmentMessage.visibility assignment when equipment list is available
         /*
@@ -89,6 +76,51 @@ class ReservationDetailsActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun initializeEquipment() {
+        if(vm.reservation.value?.equipments != null && vm.reservation.value?.equipments!!.isNotEmpty()) {
+            for ((index, e) in vm.reservation.value?.equipments!!.withIndex() ){
+                var row = layoutInflater.inflate(R.layout.equipment_row, equipment, false)
+                row.id = index
+                val equipmentName = row.findViewById<TextView>(R.id.equipmentName)
+                val equipmentQuantity = row.findViewById<TextView>(R.id.equipmentQuantity)
+                val equipmentPrice = row.findViewById<TextView>(R.id.equipmentPrice)
+
+                equipmentName.text = "Equipment ${e.equipmentId}"
+                equipmentQuantity.text = String.format("%d", e.quantity)
+                equipmentPrice.text = String.format("%.2f", e.totalPrice)
+
+                equipment.addView(row, index)
+            }
+        }
+    }
+
+    private fun initializeValues() {
+        reservationNumber.text = "Reservation number: " + String.format("%010d", vm.reservation.value?.id)
+        reservationDate.text = vm.reservation.value?.date?.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))
+        reservationStartTime.text = vm.reservation.value?.startTime?.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
+        reservationEndTime.text = vm.reservation.value?.endTime?.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
+        reservationSport.text = vm.reservation.value?.sportName
+        reservationPlayground.text = vm.reservation.value?.playgroundName
+        reservationSportCenter.text = vm.reservation.value?.sportCenterName
+        reservationSportCenterAddress.text = vm.reservation.value?.location
+        reservationTotalPrice.text = "€ " + String.format("%.2f", vm.reservation.value?.totalPrice)
+    }
+
+    private fun retrieveViews() {
+        reservationNumber = findViewById(R.id.reservationNumber)
+        reservationDate = findViewById(R.id.reservationDate)
+        reservationStartTime = findViewById(R.id.reservationStartTime)
+        reservationEndTime = findViewById(R.id.reservationEndTime)
+        reservationSport = findViewById(R.id.reservationSport)
+        reservationPlayground = findViewById(R.id.reservationPlaygroundName)
+        reservationSportCenter = findViewById(R.id.reservationSportCenter)
+        reservationSportCenterAddress = findViewById(R.id.reservationAddress)
+        directionsButton = findViewById(R.id.directionsButton)
+        noEquipmentMessage = findViewById(R.id.noEquipmentMessage)
+        reservationTotalPrice = findViewById(R.id.reservationPrice)
+        equipment = findViewById(R.id.equipmentContainer)
     }
 
     private fun handleDirectionsButton(address: String) {
--- project/app/src/main/java/it/polito/mad/sportapp/reservation_details/ReservationDetailsViewModel.kt
+++ project/app/src/main/java/it/polito/mad/sportapp/reservation_details/ReservationDetailsViewModel.kt
@@ -3,9 +3,15 @@ package it.polito.mad.sportapp.reservation_details
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import it.polito.mad.sportapp.entities.DetailedReservation
+import it.polito.mad.sportapp.model.Repository
+import javax.inject.Inject
 
-class ReservationDetailsViewModel : ViewModel() {
+@HiltViewModel
+class ReservationDetailsViewModel  @Inject constructor(
+    repository: Repository
+) : ViewModel() {
 
     private val _reservation = MutableLiveData<DetailedReservation>().also {
         it.value = mockReservationDetails()
