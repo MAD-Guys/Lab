0d46aa682a8e609650ef3daed3d7310bc7427e44
FRANCESCO ROSATI s296247 <s296247@studenti.polito.it>
2023-05-02 20:19:05 +0200

    Modified reservation details activity and edit equipment activity

--- project/app/src/main/java/it/polito/mad/sportapp/reservation_details/EditEquipmentViewModel.kt
+++ project/app/src/main/java/it/polito/mad/sportapp/reservation_details/EditEquipmentViewModel.kt
@@ -7,7 +7,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import it.polito.mad.sportapp.entities.DetailedReservation
 import it.polito.mad.sportapp.entities.Equipment
 import it.polito.mad.sportapp.entities.EquipmentReservation
-import it.polito.mad.sportapp.entities.PlaygroundReservation
 import it.polito.mad.sportapp.model.Repository
 import javax.inject.Inject
 
@@ -19,43 +18,22 @@ class EditEquipmentViewModel @Inject constructor(
     private var _reservation = MutableLiveData<DetailedReservation>()
     val reservation: LiveData<DetailedReservation> = _reservation
 
-    /*
-    private val _availableEquipment = MutableLiveData<MutableList<Equipment>>().also {
-        it.value = mockAvailableEquipment()
-    }
-
-     */
-    private val _availableEquipment = MutableLiveData<MutableList<Equipment>>()
+    private var _availableEquipment: MutableList<Equipment> = mutableListOf()
+    private var isAvailableListInitialized = false
 
     //items to display in the "available equipments" section: remove the items already selected
 
-    private val _tempAvailableEquipment: MutableLiveData<MutableList<Equipment>> =
-        MutableLiveData<MutableList<Equipment>>(
-            _availableEquipment.value?.filter {
-                var result = true
-                for (e in _reservation.value?.equipments!!) {
-                    if (it.id == e.equipmentId) result = false
-                }
-                return@filter result
-            }?.toMutableList()
-        )
+    private val _tempAvailableEquipment = MutableLiveData<MutableList<Equipment>>()
 
-
-    //private val _tempAvailableEquipment: MutableLiveData<MutableList<Equipment>> = MutableLiveData<MutableList<Equipment>>()
     val tempAvailableEquipment: MutableLiveData<MutableList<Equipment>> = _tempAvailableEquipment
 
+    private val _tempSelectedEquipment = MutableLiveData<MutableList<EquipmentReservation>>()
+    private var isSelectedEquipmentInitialized = false
 
-    private val _tempSelectedEquipment: MutableLiveData<MutableList<EquipmentReservation>> =
-        MutableLiveData<MutableList<EquipmentReservation>>(
-            _reservation.value?.equipments?.toMutableList()
-        )
-
-
-    //private val _tempSelectedEquipment: MutableLiveData<MutableList<EquipmentReservation>> = MutableLiveData<MutableList<EquipmentReservation>>()
     val tempSelectedEquipment: MutableLiveData<MutableList<EquipmentReservation>> =
         _tempSelectedEquipment
 
-    var tempPrice: MutableLiveData<Float> = MutableLiveData<Float>(_reservation.value?.totalPrice)
+    var tempPrice: MutableLiveData<Float> = MutableLiveData<Float>()
     private val dirty: MutableSet<Int> = mutableSetOf()
     private val removed: MutableSet<Int> = mutableSetOf()
 
@@ -72,61 +50,92 @@ class EditEquipmentViewModel @Inject constructor(
 
     fun getEquipmentFromDb(r: DetailedReservation) {
 
+        tempPrice.value = r.totalPrice
         val dbThread = Thread {
-            this._availableEquipment.postValue(
+            this._tempAvailableEquipment.postValue(
                 repository.getEquipmentBySportCenterIdAndSportId(
                     r.sportCenterId,
                     r.sportId
                 )
             )
-            /*
-            _tempAvailableEquipment.postValue(
-                (_availableEquipment.value?.filter {
-                    var result = true
-                    for (e in _reservation.value?.equipments!!) {
-                        if (it.id == e.equipmentId) result = false
-                    }
-                    return@filter result
-                } as MutableList<Equipment>)
-            )
-            _tempSelectedEquipment.postValue(
-                _reservation.value?.equipments as MutableList<EquipmentReservation>
-            )
-            */
-
         }
         dbThread.start()
     }
 
-    fun addEquipment(playgroundReservationId: Int, equipmentId: Int, price: Float) {
-        _tempSelectedEquipment.value!!.add(
-            EquipmentReservation(
-                -1,
-                playgroundReservationId,
-                equipmentId,
-                1,
-                "timestamp",
-                price
+    fun initAvailableEquipments() {
+
+        if (
+            !isAvailableListInitialized
+            && _tempAvailableEquipment.value?.isNotEmpty() == true
+        ) {
+            _availableEquipment.addAll(_tempAvailableEquipment.value!!)
+
+            _tempAvailableEquipment.value!!.filter {
+                var result = true
+                for (e in _reservation.value?.equipments!!) {
+                    if (it.id == e.equipmentId) result = false
+                }
+                return@filter result
+            }
+            isAvailableListInitialized = true
+        }
+    }
+
+    fun initSelectedEquipments() {
+        if (
+            !isSelectedEquipmentInitialized
+            && _reservation.value?.equipments?.isNotEmpty() == true
+        ) {
+            _tempSelectedEquipment.postValue(
+                _reservation.value?.equipments?.toMutableList() ?: mutableListOf()
             )
+        }
+        isSelectedEquipmentInitialized = true
+    }
+
+    fun addEquipment(
+        playgroundReservationId: Int,
+        equipmentId: Int,
+        price: Float,
+        equipmentName: String
+    ) {
+        if (_tempSelectedEquipment.value == null) {
+            _tempSelectedEquipment.value = mutableListOf()
+        }
+        val newReservation = EquipmentReservation(
+            -1,
+            playgroundReservationId,
+            equipmentId,
+            1,
+            "timestamp",
+            price
+        )
+        newReservation.equipmentName = equipmentName
+        _tempSelectedEquipment.value!!.add(
+            newReservation
         )
         _tempAvailableEquipment.value?.removeIf { it.id == equipmentId }
         tempPrice.value = tempPrice.value?.plus(price) ?: price
 
-        tempAvailableEquipment.setValue(_tempAvailableEquipment.value)
-        tempSelectedEquipment.setValue(_tempSelectedEquipment.value)
+        tempAvailableEquipment.value = _tempAvailableEquipment.value
+        tempSelectedEquipment.value = _tempSelectedEquipment.value
     }
 
     private fun removeEquipment(equipmentId: Int) {
         val index =
             _tempSelectedEquipment.value!!.indexOf(_tempSelectedEquipment.value!!.find { it.equipmentId == equipmentId })
+
         removed.add(_tempSelectedEquipment.value!![index].id)
-        _tempAvailableEquipment.value?.add(_availableEquipment.value?.find { it.id == equipmentId }!!)
+
+        _tempAvailableEquipment.value?.add(_availableEquipment.find { it.id == equipmentId }!!)
+
         tempPrice.value = tempPrice.value?.minus(_tempSelectedEquipment.value!![index].totalPrice)
             ?: tempPrice.value
+
         _tempSelectedEquipment.value!!.removeAt(index)
 
-        tempAvailableEquipment.setValue(_tempAvailableEquipment.value)
-        tempSelectedEquipment.setValue(_tempSelectedEquipment.value)
+        tempAvailableEquipment.value = _tempAvailableEquipment.value
+        tempSelectedEquipment.value = _tempSelectedEquipment.value
     }
 
     fun incrementQuantity(equipmentId: Int): Boolean {
@@ -135,10 +144,10 @@ class EditEquipmentViewModel @Inject constructor(
         val index = _tempSelectedEquipment.value!!.indexOf(current)
         val newPrice = (current!!.totalPrice / current.quantity) * (current.quantity + 1)
         tempPrice.value =
-            tempPrice.value?.minus(current!!.totalPrice)?.plus(newPrice) ?: tempPrice.value
+            tempPrice.value?.minus(current.totalPrice)?.plus(newPrice) ?: tempPrice.value
 
-        if (current.quantity < _availableEquipment.value?.find { it.id == equipmentId }!!.availability) {
-            _tempSelectedEquipment.value!![index] = EquipmentReservation(
+        if (current.quantity < _availableEquipment.find { it.id == equipmentId }!!.availability) {
+            val newReservation = EquipmentReservation(
                 current.id,
                 current.playgroundReservationId,
                 equipmentId,
@@ -146,6 +155,8 @@ class EditEquipmentViewModel @Inject constructor(
                 current.timestamp,
                 newPrice
             )
+            newReservation.equipmentName = current.equipmentName
+            _tempSelectedEquipment.value!![index] = newReservation
 
             dirty.add(current.id)
             tempSelectedEquipment.setValue(_tempSelectedEquipment.value)
@@ -160,28 +171,32 @@ class EditEquipmentViewModel @Inject constructor(
 
         if (current!!.quantity > 1) {
             val index = _tempSelectedEquipment.value!!.indexOf(current)
-            val newPrice = (current!!.totalPrice / current.quantity) * (current.quantity - 1)
+            val newPrice = (current.totalPrice / current.quantity) * (current.quantity - 1)
             tempPrice.value =
                 tempPrice.value?.minus(current.totalPrice)?.plus(newPrice) ?: tempPrice.value
-            _tempSelectedEquipment.value!![index] = EquipmentReservation(
+            val newReservation = EquipmentReservation(
                 current.id,
-                current!!.playgroundReservationId,
+                current.playgroundReservationId,
                 equipmentId,
-                current!!.quantity - 1,
+                current.quantity - 1,
                 current.timestamp,
                 newPrice
             )
+            newReservation.equipmentName = current.equipmentName
+            _tempSelectedEquipment.value!![index] = newReservation
 
             dirty.add(current.id)
         } else {
             removeEquipment(equipmentId) //when quantity becomes zero
         }
-        tempSelectedEquipment.setValue(_tempSelectedEquipment.value)
+
+        tempSelectedEquipment.value = _tempSelectedEquipment.value
     }
 
     fun saveEquipment() {
 
         val dbThread = Thread {
+
             //Remove the removed
             for (i in removed) {
                 if (i != -1) { //if the item was already saved
@@ -190,6 +205,7 @@ class EditEquipmentViewModel @Inject constructor(
                     repository.deleteEquipmentReservation(equipment!!)
                 }
             }
+
             //update the updated
             for (i in dirty) {
                 if (i != -1) { //if the item was already saved
@@ -203,19 +219,26 @@ class EditEquipmentViewModel @Inject constructor(
                     )
                 }
             }
-            //add the added
-            for (e in tempSelectedEquipment.value!!) {
-                if (e.id == -1) { //if the item was not already saved
-                    //call repository to add
-                    val equipment = _availableEquipment.value!!.find { it.id == e.equipmentId }
-                    repository.addEquipmentReservation(
-                        equipment!!,
-                        e.quantity,
-                        e.playgroundReservationId
-                    )
+
+            tempSelectedEquipment.value?.let {
+                //add the added
+                for (e in tempSelectedEquipment.value!!) {
+                    if (e.id == -1) { //if the item was not already saved
+                        //call repository to add
+                        val equipment = _availableEquipment.find { it.id == e.equipmentId }
+
+                        equipment?.let {
+                            repository.addEquipmentReservation(
+                                equipment,
+                                e.quantity,
+                                e.playgroundReservationId
+                            )
+                        }
+                    }
                 }
             }
         }
+
         dbThread.start()
     }
 
